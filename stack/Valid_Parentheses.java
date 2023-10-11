f593609c33c8ead69f85b7a7d0944faa1709943d
Mukul <mukul1singh23@gmail.com>
2023-10-11 12:26:11 +0530

    leetcode 20

---
+++ stack/Valid_Parentheses.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Valid_Parentheses {
+  public boolean isValid(String s) {
+    Deque<Character> stack = new ArrayDeque<>();
+
+    for (final char c : s.toCharArray())
+      if (c == '(')
+        stack.push(')');
+      else if (c == '{')
+        stack.push('}');
+      else if (c == '[')
+        stack.push(']');
+      else if (stack.isEmpty() || stack.pop() != c)
+        return false;
+
+    return stack.isEmpty();
+  }
+}
